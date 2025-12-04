385126078163de782e89c6287d812efe179e194e
HyeontaeKim0 <hyeontae.kim@wincl.io>
2025-12-04 14:36:20 +0900

    feat : notfound 추가

---
+++ components/notFound/NotFound.tsx
@@ -0,0 +1,20 @@
+import { Loader } from "lucide-react";
+import NotFoundIcon from "../../assets/img/error/notFound.png";
+export default function NotFound({ text }: { text: string }) {
+  return (
+    <div className="flex flex-col items-center justify-center space-x-2 mt-10">
+      {/* 회전 하는 로딩 아이콘 */}
+      <div className="animate-spin-slow absolute top-[115px] left-[43%] ">
+        <Loader className="w-[55px] h-[55px] text-gray-400 " />
+      </div>
+      <img
+        src={NotFoundIcon.src}
+        alt="not found"
+        className="w-[150px]  object-contain"
+      />
+      <span className="text-[12px] mt-5 font-mono font-bold text-gray-400 text-center">
+        {text}
+      </span>
+    </div>
+  );
+}
--- components/raceResults/components/DataTable.tsx
+++ components/raceResults/components/DataTable.tsx
@@ -1,8 +1,8 @@
 import type { DriverResult } from "@/lib/types/types";
 import { getTeamLogoUrl } from "@/lib/utils/driverUtils";
+import { Trophy, Award, Clock, Loader } from "lucide-react";
 
-import { Trophy, Award, Clock } from "lucide-react";
-
+import NotFound from "../../notFound/NotFound";
 export default function DataTable({
   raceResults,
   setHoveredRow,
@@ -39,182 +39,202 @@ export default function DataTable({
     }
   };
 
+  console.log("raceResults", raceResults);
+
   return (
     <div className="overflow-x-auto -mx-6 px-6">
       <table className="w-full">
         <thead>
           <tr className="border-b border-gray-200">
-            {getTableHeaders()?.map((header) => (
-              <th className="text-left py-4 px-4 text-xs font-extrabold text-gray-400 uppercase tracking-wider">
-                {header}
-              </th>
-            ))}
+            {getTableHeaders()?.map((header) =>
+              // "랩","포인트"일때는 text-center
+              header === "랩" || header === "포인트" ? (
+                <th className="text-center py-4 px-4 text-xs font-extrabold text-gray-400 uppercase tracking-wider">
+                  {header}
+                </th>
+              ) : (
+                <th className="text-left py-4 px-4 text-xs font-extrabold text-gray-400 uppercase tracking-wider">
+                  {header}
+                </th>
+              )
+            )}
           </tr>
         </thead>
         <tbody>
-          {raceResults.map((result, index) => (
-            <tr
-              key={`${result.position}-${index}`}
-              className="border-b border-gray-200 hover:bg-gray-50 transition-all duration-300 group cursor-pointer relative"
-              onMouseEnter={() => setHoveredRow(result.position)}
-              onMouseLeave={() => setHoveredRow(null)}
-              style={{
-                animationDelay: `${index * 0.05}s`,
-              }}
-            >
-              <td className="py-4 px-4">
-                <div className="flex items-center space-x-2">
-                  <span
-                    className={`font-bold text-lg ${getPositionColor(
-                      result.position
-                    )}`}
-                  >
-                    {(result as any).originalPosition ?? result.position}
-                  </span>
-                  {result.position === 1 &&
-                    typeof (result as any).originalPosition === "number" && (
-                      <Trophy
-                        className={`${getPositionColor(
-                          result.position
-                        )} animate-pulse-slow`}
-                        size={18}
-                      />
-                    )}
-                  {result.position === 2 &&
-                    typeof (result as any).originalPosition === "number" && (
-                      <Award
-                        className={getPositionColor(result.position)}
-                        size={18}
-                      />
-                    )}
-                  {result.position === 3 &&
-                    typeof (result as any).originalPosition === "number" && (
-                      <Award
-                        className={getPositionColor(result.position)}
-                        size={18}
-                      />
-                    )}
-                </div>
+          {raceResults.length === 0 ? (
+            <tr>
+              <td
+                colSpan={getTableHeaders()?.length}
+                className="text-center py-4 px-4"
+              >
+                <NotFound text="경기가 없거나 경기 전 혹은 데이터 수집 중 입니다....." />
               </td>
-              <td className="py-4 px-4">
-                <div className="flex items-center space-x-3">
-                  {result.teamColor && (
-                    <div
-                      className="w-1.5 h-12 rounded-full transition-all duration-300 group-hover:shadow-lg"
-                      style={{
-                        backgroundColor: result.teamColor,
-                        boxShadow:
-                          hoveredRow === result.position
-                            ? `0 0 15px ${result.teamColor}80`
-                            : "none",
-                      }}
-                    ></div>
-                  )}
-                  <div>
-                    <div className="font-semibold text-sm  text-gray-900 group-hover:text-primary transition-colors duration-300">
-                      {result.driverName}
-                    </div>
-                    <div className="flex items-center gap-[7px]">
-                      <div className="text-sm text-gray-600 font-medium">
-                        {result.team}
-                      </div>
-                      {getTeamLogoUrl(Number(result.driverNumber || "0")) && (
-                        <div className="mt-1">
-                          <img
-                            src={getTeamLogoUrl(
-                              Number(result.driverNumber || "0")
-                            )}
-                            alt={`${result.team} 로고`}
-                            className="h-[23px] w-[23px] object-contain"
-                          />
-                        </div>
+            </tr>
+          ) : (
+            raceResults.map((result, index) => (
+              <tr
+                key={`${result.position}-${index}`}
+                className="border-b border-gray-200 hover:bg-gray-50 transition-all duration-300 group cursor-pointer relative"
+                onMouseEnter={() => setHoveredRow(result.position)}
+                onMouseLeave={() => setHoveredRow(null)}
+                style={{
+                  animationDelay: `${index * 0.05}s`,
+                }}
+              >
+                <td className="py-4 px-4">
+                  <div className="flex items-center space-x-2">
+                    <span
+                      className={`font-bold text-lg ${getPositionColor(
+                        result.position
+                      )}`}
+                    >
+                      {(result as any).originalPosition ?? result.position}
+                    </span>
+                    {result.position === 1 &&
+                      typeof (result as any).originalPosition === "number" && (
+                        <Trophy
+                          className={`${getPositionColor(
+                            result.position
+                          )} animate-pulse-slow`}
+                          size={18}
+                        />
                       )}
+                    {result.position === 2 &&
+                      typeof (result as any).originalPosition === "number" && (
+                        <Award
+                          className={getPositionColor(result.position)}
+                          size={18}
+                        />
+                      )}
+                    {result.position === 3 &&
+                      typeof (result as any).originalPosition === "number" && (
+                        <Award
+                          className={getPositionColor(result.position)}
+                          size={18}
+                        />
+                      )}
+                  </div>
+                </td>
+                <td className="py-4 px-4">
+                  <div className="flex items-center space-x-3">
+                    {result.teamColor && (
+                      <div
+                        className="w-1.5 h-12 rounded-full transition-all duration-300 group-hover:shadow-lg"
+                        style={{
+                          backgroundColor: result.teamColor,
+                          boxShadow:
+                            hoveredRow === result.position
+                              ? `0 0 15px ${result.teamColor}80`
+                              : "none",
+                        }}
+                      ></div>
+                    )}
+                    <div>
+                      <div className="font-semibold text-sm  text-gray-900 group-hover:text-primary transition-colors duration-300">
+                        {result.driverName}
+                      </div>
+                      <div className="flex items-center gap-[7px]">
+                        <div className="text-sm text-gray-600 font-medium">
+                          {result.team}
+                        </div>
+                        {getTeamLogoUrl(Number(result.driverNumber || "0")) && (
+                          <div className="mt-1">
+                            <img
+                              src={getTeamLogoUrl(
+                                Number(result.driverNumber || "0")
+                              )}
+                              alt={`${result.team} 로고`}
+                              className="h-[23px] w-[23px] object-contain"
+                            />
+                          </div>
+                        )}
+                      </div>
                     </div>
                   </div>
-                </div>
-              </td>
-              {view === "qualifying" ? (
-                <>
-                  <td className="py-4 px-1">
-                    <div className="flex items-center space-x-1.5">
-                      <Clock size={14} className="text-gray-400" />
-                      <span className="text-sm font-mono font-medium">
-                        {result.time || "-"}
-                      </span>
-                    </div>
-                  </td>
-                  <td className="py-4 px-1">
-                    <div className="flex items-center space-x-1.5">
-                      <Clock size={14} className="text-gray-400" />
-                      <span className="text-sm font-mono font-medium">
-                        {result.time2 || "-"}
-                      </span>
-                    </div>
-                  </td>
-                  <td className="py-4 px-1">
-                    <div className="flex items-center space-x-1.5">
-                      <Clock size={14} className="text-gray-400" />
-                      <span className="text-sm font-mono font-medium">
-                        {result.time3 || "-"}
-                      </span>
-                    </div>
-                  </td>
-                </>
-              ) : (
-                <>
-                  {view === "practice" ? (
-                    <>
-                      <td className="py-4 px-1">
-                        <div className="flex items-center space-x-1.5">
-                          <Clock size={14} className="text-gray-400" />
-                          <span className="text-sm font-mono font-medium">
-                            {result.time || "-"}
-                          </span>
-                        </div>
-                      </td>
-                      <td className="py-4 px-1">
-                        <div className="flex items-center space-x-1.5">
-                          <Clock size={14} className="text-gray-400" />
-                          <span className="text-sm font-mono font-medium">
-                            {result.time2 || "-"}
-                          </span>
-                        </div>
-                      </td>
-                      <td className="py-4 px-1">
-                        <div className="flex items-center space-x-1.5">
-                          <Clock size={14} className="text-gray-400" />
-                          <span className="text-sm font-mono font-medium">
-                            {result.time3 || "-"}
-                          </span>
-                        </div>
-                      </td>
-                    </>
-                  ) : (
-                    <>
-                      <td className="py-4 px-4">
-                        <div className="flex items-center space-x-1.5">
-                          <Clock size={14} className="text-gray-400" />
-                          <span className="text-sm font-mono font-medium">
-                            {result.time || "-"}
-                          </span>
-                        </div>
-                      </td>
-                      <td className="py-4 px-4 text-center text-sm text-gray-700 font-semibold">
-                        {result.laps}
-                      </td>
-                      <td className="py-4 px-4 text-center">
-                        <div className="flex items-center justify-center space-x-2">
-                          <span className="font-bold text-lg text-primary">
-                            {result.points}
-                          </span>
-                        </div>
-                      </td>
-                    </>
-                  )}
-                </>
-              )}
-            </tr>
-          ))}
+                </td>
+                {view === "qualifying" ? (
+                  <>
+                    <td className="py-4 px-1">
+                      <div className="flex items-center space-x-1.5">
+                        <Clock size={14} className="text-gray-400" />
+                        <span className="text-sm font-mono font-medium">
+                          {result.time || "-"}
+                        </span>
+                      </div>
+                    </td>
+                    <td className="py-4 px-1">
+                      <div className="flex items-center space-x-1.5">
+                        <Clock size={14} className="text-gray-400" />
+                        <span className="text-sm font-mono font-medium">
+                          {result.time2 || "-"}
+                        </span>
+                      </div>
+                    </td>
+                    <td className="py-4 px-1">
+                      <div className="flex items-center space-x-1.5">
+                        <Clock size={14} className="text-gray-400" />
+                        <span className="text-sm font-mono font-medium">
+                          {result.time3 || "-"}
+                        </span>
+                      </div>
+                    </td>
+                  </>
+                ) : (
+                  <>
+                    {view === "practice" ? (
+                      <>
+                        <td className="py-4 px-1">
+                          <div className="flex items-center space-x-1.5">
+                            <Clock size={14} className="text-gray-400" />
+                            <span className="text-sm font-mono font-medium">
+                              {result.time || "-"}
+                            </span>
+                          </div>
+                        </td>
+                        <td className="py-4 px-1">
+                          <div className="flex items-center space-x-1.5">
+                            <Clock size={14} className="text-gray-400" />
+                            <span className="text-sm font-mono font-medium">
+                              {result.time2 || "-"}
+                            </span>
+                          </div>
+                        </td>
+                        <td className="py-4 px-1">
+                          <div className="flex items-center space-x-1.5">
+                            <Clock size={14} className="text-gray-400" />
+                            <span className="text-sm font-mono font-medium">
+                              {result.time3 || "-"}
+                            </span>
+                          </div>
+                        </td>
+                      </>
+                    ) : (
+                      <>
+                        <td className="py-4 px-4">
+                          <div className="flex items-center space-x-1.5">
+                            <Clock size={14} className="text-gray-400" />
+                            <span className="text-sm font-mono font-medium">
+                              {result.time || "-"}
+                            </span>
+                          </div>
+                        </td>
+                        <td className="py-4 px-4 text-center text-sm text-gray-700 font-semibold">
+                          {result.laps}
+                        </td>
+                        <td className="py-4 px-4 text-center">
+                          <div className="flex items-center justify-center space-x-2">
+                            <span className="font-bold text-lg text-primary">
+                              {result.points}
+                            </span>
+                          </div>
+                        </td>
+                      </>
+                    )}
+                  </>
+                )}
+              </tr>
+            ))
+          )}
         </tbody>
       </table>
     </div>
