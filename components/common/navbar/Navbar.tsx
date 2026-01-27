95aa9c4f68d192b75d8b3dd136d21b54670c6178
HyeontaeKim0 <hyeontae.kim@wincl.io>
2026-01-27 16:19:35 +0900

    fix : navbar 수정

--- components/common/navbar/Navbar.tsx
+++ components/common/navbar/Navbar.tsx
@@ -21,9 +21,10 @@ export default function Navbar() {
   const navItems = [
     { label: "홈", href: "/" },
     { label: "결과", href: "/drivers" },
-    // { label: "게시판", href: "/board" },
-    { label: "뉴스", href: "/news" },
     { label: "일정", href: "/schedule" },
+    // { label: "밈생성기", href: "/memeGenerator" },
+    // { label: "게시판", href: "/board" },
+    // { label: "뉴스", href: "/news" },
     // { label: "결과", href: "/results" },
     // { label: "순위", href: "/standings" },
     // { label: "팀", href: "/teams" },
