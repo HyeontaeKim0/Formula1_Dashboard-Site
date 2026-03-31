c5de83650f7883bc536af40a9cc727bfaedfff5f
HyeontaeKim0 <hyeontae.kim@wincl.io>
2026-03-31 09:57:58 +0900

    feat : 페이지 타이틀 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/common/footer/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "F1 Dashboard | 포뮬러1 대시보드",
+  title: "F1 Dashboard",
   description: "포뮬러1 레이스 일정, 결과, 순위, 드라이버 정보를 한눈에",
 };
 
